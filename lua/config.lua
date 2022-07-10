f23beaa634993732957083243fd40b4216be5221
Victor Faner <victor@faner.co>
2022-07-10 14:14:18 -0500

    Fix lsp formatting map

--- lua/config.lua
+++ lua/config.lua
@@ -46,7 +46,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<space>fo', vim.lsp.buf.formatting, bufopts)
 end
 
 -- LSP-built-in snippet support
