e89841bbb870c3a8b8769204ff603cce65fe7003
Victor Faner <victor@faner.co>
2023-02-02 20:18:49 -0600

    Update cmp to source from all buffers

--- plugins.lua
+++ plugins.lua
@@ -139,7 +139,14 @@ cmp.setup({
     { name = 'nvim_lsp_signature_help' },
     { name = 'vim-dadbod-completion' },
     { name = 'luasnip' },
-    { name = 'buffer' },
+    {
+      name = 'buffer',
+      option = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end
+      }
+    },
     { name = 'path' },
     { name = 'treesitter' }
   }),
