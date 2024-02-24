4503a355c12e094f134cfc6483d0a58836070505
Victor Faner <victor@faner.co>
2024-02-24 07:57:42 -0600

    Change vim-sandwich to nvim-surround

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -3,10 +3,10 @@ vim.g.sandwich_no_default_key_mappings = 1
 return {
   --------------------------------- Surround -----------------------------------
   {
-    "machakann/vim-sandwich",
-    config = function ()
-      vim.cmd[[runtime macros/sandwich/keymap/surround.vim]]
-    end
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function() require("nvim-surround").setup({}) end
   },
 
   ------------------------------ Text alignment --------------------------------
